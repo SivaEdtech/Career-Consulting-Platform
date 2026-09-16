889d035cd2baf6980aef918d4bebc579c87497c8
Keshav Gupta <gupta.keshav0406@gmail.com>
2026-09-16 18:40:45 +0530

    validator added

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import passport from "../config/passport.js";
 import { register, login, googleCallback} from "../controllers/auth.controller.js"
+import {registerUserValidator, loginUserValidator} from "../middlewares/validator.middleware.js"
 
 const router = Router();
 
 // Normal authentication
-router.post("/auth/register", register);
-router.post("/auth/login", login);
+router.post("/auth/register",registerUserValidator, register);
+router.post("/auth/login",loginUserValidator, login);
 
 // Google authentication
 router.get(
